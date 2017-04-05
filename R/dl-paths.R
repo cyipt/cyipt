98b244fe32401a59f7e6c1afd9aa9073175996ba
Robin Lovelace <rob00x@gmail.com>
2017-04-05 15:53:15 +0100

    Update dl-scripts - link to cs data

--- R/dl-paths.R
+++ R/dl-paths.R
@@ -32,16 +32,38 @@ ct = osmdata_sp(qct)
 
 # la data
 library(sf)
-library(dplyr)
-las = st_read("~/npct/pct-bigdata/las-pcycle.geojson")
-summary(las)
-bristol = las[grepl(pattern = "Bristol", las$NAME),]
-plot(bristol)
 library(tmap)
+library(dplyr)
+library(osmdata)
+# las = st_read("~/npct/pct-bigdata/las-pcycle.geojson")
+# summary(las)
+# bristol = las[grepl(pattern = "Bristol", las$NAME),]
+# plot(bristol)
+# bristol = st_read("http://geoportal.statistics.gov.uk/datasets/686603e943f948acaa13fb5d2b0f1275_3.kml")
+# bristol = filter(bristol, grepl("Bristol", lad16nm))
+# st_write(bristol, "areas/bristol-poly.geojson", update = TRUE)
+bristol = st_read("areas/bristol-poly.geojson")
 tmap_mode("view")
 qtm(bristol)
-bristol = st_read("http://geoportal.statistics.gov.uk/datasets/686603e943f948acaa13fb5d2b0f1275_3.kml")
-bristol = filter(bristol, grepl("Bristol", lad16nm))
-qtm(bristol)
-st_write(bristol, "areas/bristol-poly.geojson", update = TRUE)
+q = opq(st_bbox(bristol)) %>%
+  add_feature(key = "highway", value = "[a-z]", value_exact = FALSE)
+res = osmdata_sf(q = q)
+plot(res$osm_lines[1])
 
+cyclestreets_attributes = readr::read_csv("input-data/scorings/bristol.csv")
+head(cyclestreets_attributes)
+head(res$osm_lines)
+class(res$osm_lines$osm_id)
+res$osm_lines$osm_id = as.integer(as.character(res$osm_lines$osm_id))
+cyclestreets_attributes = rename(cyclestreets_attributes, osm_id = id)
+osml = left_join(res$osm_lines, cyclestreets_attributes)
+
+osml_full = osml
+osmp_sub = osmdata_sf(q = opq(bbox = "Broadmead") %>% add_feature(key = "building", value = "house"))
+qtm(osmp_sub$osm_polygons[2:15,])
+bb_square = stplanr::bb2poly(as(osmp_sub$osm_polygons[2:15,], "Spatial"))
+osml = osml_full[st_as_sf(bb_square),]
+qtm(osml, lines.col = "quietness")
+dir.create("../example-data/bristol")
+# st_write(osml, "../example-data/bristol/osm-lines-quietness.gpkg") # fails
+saveRDS(osml, "../example-data/bristol/osm-lines-quietness.Rds")
