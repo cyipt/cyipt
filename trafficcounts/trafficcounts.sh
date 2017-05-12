3a76e30d3306b89f0eb7e2d202f5f4550da1e684
mvl22 <martin@lucas-smith.co.uk>
2017-05-12 23:51:24 +0100

    Add download mechanics for AADF data files

--- trafficcounts/trafficcounts.sh
+++ trafficcounts/trafficcounts.sh
@@ -12,17 +12,23 @@
 # Settings
 
 # Download URL for "Major road network - shape file format"
-downloadUrl=http://data.dft.gov.uk/gb-traffic-matrix/major-roads-link-network2016.zip
-
+downloadUrlNetwork=http://data.dft.gov.uk/gb-traffic-matrix/major-roads-link-network2016.zip
+downloadUrlMajorRoadsByDirection=http://data.dft.gov.uk/gb-traffic-matrix/AADF-data-by-direction-major-roads.zip
+downloadUrlMajorRoads=http://data.dft.gov.uk/gb-traffic-matrix/AADF-data-major-roads.zip
+downloadUrlMinorRoads=http://data.dft.gov.uk/gb-traffic-matrix/AADF-data-minor-roads.zip
 
 
 # Set to terminate if error
 set -e
 
+#
+# ROAD NETWORK STRUCTURE DATA
+#
+
 # Download the data if not already downloaded
 zipFile=major-roads-link-network.zip
 if [ ! -f $zipFile ] ; then
-	wget -O $zipFile $downloadUrl
+	wget -O $zipFile $downloadUrlNetwork
 fi
 
 # Unzip the data to a directory containing the shapefile files
@@ -59,3 +65,28 @@ sed -i '' -E 's/^(.+),(.+),(.+),(.+),(.+),(.+)/\5,\6,\1,\2,\3,\4/g' $csvFile
 
 # Confirm completion
 echo "Data successfully downloaded and converted to CSV file now at $csvFile"
+
+
+
+#
+# ROAD DATA
+#
+
+# Download and unpack each dataset
+files=( $downloadUrlMajorRoadsByDirection $downloadUrlMajorRoads $downloadUrlMinorRoadsByDirection )
+for file in $files ; do
+	
+	# Download the zip file if not already downloaded
+	zipFile=`basename $file`
+	if [ ! -f $zipFile ] ; then
+		wget $file
+	fi
+	
+	# Unzip the zip file
+	dataFile="${zipFile%.zip}.csv"
+	if [ ! -f $dataFile ] ; then
+		unzip -q $dataFile
+	fi
+	
+done
+
