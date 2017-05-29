554785b6db70de4a24eb3a04c7ce351f6f75d8c1
Robin Lovelace <rob00x@gmail.com>
2017-05-29 17:26:51 +0100

    Add voronoi spatial join - see #43

---
+++ R/geobuffer.R
@@ -0,0 +1,33 @@
+#' Select a projected CRS
+#'
+#' @examples
+#' shp = st_sf(st_sfc(st_point(c(1, 0))))
+#' geo_select_aeq(shp)
+geo_select_aeq <- function(shp){
+  cent <- st_geometry(shp)
+  coords = st_coordinates(shp)
+  coords_mat = matrix(coords[,1:2], ncol = 2)
+  midpoint = apply(coords_mat, 2, mean)
+  aeqd <- sprintf("+proj=aeqd +lat_0=%s +lon_0=%s +x_0=0 +y_0=0",
+                  midpoint[1], midpoint[1])
+  st_crs(aeqd)
+}
+
+#' @examples
+#' shp = st_sf(st_sfc(st_point(c(1, 0))))
+#' geo_projected(shp, st_buffer, width = 100)
+geo_projected = function(shp, fun, crs_temp = geo_select_aeq(shp),  ...){
+  # assume it's not projected  (i.e. lat/lon) if there is no CRS
+  if(is.na(st_crs(shp))) st_crs(shp) = 4326
+  crs_orig = st_crs(shp)
+  shp_projected = st_transform(shp, crs_temp)
+  message(paste0("Running function on a temporary projected version of the Spatial object using the CRS: ", crs_temp$proj4string))
+  res = fun(shp_projected, ...)
+  if(is(res, "sf"))
+    res = st_transform(res, crs_orig)
+  res
+}
+
+geo_buffer = function(shp, dist = 0) {
+  geo_projected(shp = shp, fun = st_buffer, dist = dist)
+}
--- trafficcounts/test-traffic.R
+++ trafficcounts/test-traffic.R
@@ -1,14 +1,67 @@
 # Aim: test traffic counts data
 library(tidyverse)
+library(tmap)
 library(sf)
 tc = read_csv("trafficcounts/trafficcounts.csv")
+names(tc)
+sapply(tc, class)
+col_types = rep("c", times = ncol(tc))
+col_types[grepl(pattern = "pcu|[0-9]|latitude|longi", names(tc))] = "d"
+col_types = paste0(col_types, collapse = "")
+tc = read_csv("trafficcounts/trafficcounts.csv", col_types = col_types)
+
 tc # 174 vars, 33k rows
+ts = st_sfc(st_multipoint(cbind(tc$longitude, tc$latitude)))
+ts = ts %>% st_cast(to = "POINT")
+tc = st_sf(tc, ts, crs = 4326)
 
 # Explore linking variables (to assign to osm)
 summary(as.factor(tc$road)) # 10k, 1/3rd
-plot(tc$longitude, tc$latitude) # UK coverage
+plot(tc[1]) # UK coverage
 
 # Pick a single road
 roadname = "A4018"
 t1 = filter(tc, road == roadname)
-t1
+tmap_mode("view")
+qtm(t1)
+
+# Link with OSM ids
+ways = readRDS("../example-data/bristol/osm-lines-quietness-full.Rds")
+plot(ways[1:1000, 1])
+plot(t1, add = T)
+head(ways$name)
+t1$road
+sapply(ways, function(x) summary(x %in% t1$road[1])) # it's the ref col
+wt = filter(ways, ref == t1$road[1])
+qtm(wt) # a single road composed of 119 osm elements
+tbuff = geo_buffer(shp = t1, dist = 100)
+sel_buff = st_intersects(wt, tbuff, sparse = FALSE)
+
+# using a for loop (slow)
+for(i in 1:nrow(t1)) {
+  wt$pcu_100[sel_buff[,i]] = tbuff$all_motors_pcu_16[i]
+}
+
+# using a matrix (fast)
+sel_buff_any = apply(sel_buff, 1, any)
+sel_buff_which = unlist(apply(sel_buff, 1, which))
+wt$pcu_100[sel_buff_any] == tbuff$all_motors_pcu_16[sel_buff_which]
+
+# with voronoi polygons
+v = dismo::voronoi(xy = st_coordinates(t1), ext = raster::extent(st_coordinates(wt)[,1:2]))
+v_sf = as(v, "sf")
+st_crs(v_sf) = st_crs(t1)
+plot(v_sf)
+v_sf = st_join(x = v_sf, y = t1["all_motors_pcu_16"])
+wt$pcu_v = st_join(wt, v_sf["all_motors_pcu_16"], FUN = mean)$all_motors_pcu_16
+plot(wt, add = T)
+
+# plot results
+qtm(wt, lines.col = "pcu_100", lines.lwd = 30) +
+  qtm(wt, lines.col = "pcu_v", lines.lwd = 10)
+
+wt_out = select(wt, osm_id, pcu_100, pcu_v)
+st_geometry(wt_out) = NULL
+write_csv(wt_out, "pcu_data_osm.csv")
+
+# for all counters in Bristol
