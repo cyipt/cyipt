3c9d74a53c59d910c95debecbd8f32d7c1cac299
mvl22 <martin@lucas-smith.co.uk>
2017-08-16 11:16:16 +0100

    Postgres tutorial: Add implementation example

--- postgres-test/postgres.php
+++ postgres-test/postgres.php
@@ -46,4 +46,59 @@ php postgres.php
 */
 
 
+
+# Define the settings, using the credentials above
+$settings = array (
+	'hostname' => 'localhost',
+	'username' => 'cyipt',
+	'password' => 'cyipttest',
+	'database' => 'cyipt',
+);
+
+# Connect to the database
+# We use the PDO database abstraction library, and provide a DSN connection string in this format: 'pgsql:host=localhost;dbname=example'
+try {
+	$databaseConnection = new PDO ("pgsql:host={$settings['hostname']};dbname={$settings['database']}", $settings['username'], $settings['password']);
+	$databaseConnection->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	print "Error!: " . $e->getMessage ();
+	die;
+}
+
+# Create a table (if not already present)
+$query = "
+	CREATE TABLE IF NOT EXISTS people (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(255) NOT NULL,
+		location VARCHAR(255) NOT NULL,
+		created_at TIMESTAMP DEFAULT NOW()
+	);
+";
+try {
+	$databaseConnection->query ($query);
+} catch (PDOException $e) {
+	echo "Error!: " . $e->getMessage ();
+	die;
+}
+
+# Insert data
+$query = "
+	INSERT INTO people (name, location)
+	VALUES
+		('Martin', 'Cambridge'),
+		('Robin', 'Leeds'),
+		('Malcolm', 'Leeds');
+";
+$databaseConnection->query ($query);
+
+# Select the data
+$query = "SELECT * FROM people;";
+foreach ($databaseConnection->query ($query) as $row) {
+	echo "{$row['name']} is from {$row['location']}; record created at {$row['created_at']}\n";
+}
+
+# Delete all existing data
+$query = "DELETE FROM people;";
+$databaseConnection->query ($query);
+
 ?>
