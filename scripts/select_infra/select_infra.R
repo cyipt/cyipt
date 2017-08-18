52eb4ea46fa4cb7c022f2c416e19a2f604e4f34a
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2017-08-18 15:39:28 +0100

    Code Tidy in preparation for select_infra

--- scripts/select_infra/select_infra.R
+++ scripts/select_infra/select_infra.R
@@ -1,84 +1,74 @@
-#Demo Infrastrucutre data
+#Gets PCT Values for the road segments
+
+############################################
+#NOTE: THIS OVERRIGHTS EXISTING FILES RATHER THAN CREATING NEW FILES
+#############################################
+
 library(sf)
 library(dplyr)
-#library(gdata)
+library(parallel)
 
-#Read in Data
-osm <- readRDS("../example-data/cambridge/results/osm-lines.Rds")
-vars <- read.csv("../example-data/cambridge/results/osm-variables.csv")
-pct <- read.csv("../example-data/cambridge/results/pct-census.csv")
-#width <- read.csv("../example-data/cambridge/results/widths.csv")
-#traffic <- read.csv("../cyipt/trafficcounts/trafficcounts-osm.csv")
+
+#Settings now come from master file
+#skip <- FALSE #Skip Files that already have PCT values
+#ncores <- 4 #number of cores to use in parallel processing
+#overwrite <- FALSE #Overwrite or create new file
+
+#Functions
+
+
+
+
+#List folders
+#regions <- list.dirs(path = "../cyipt-bigdata/osm-raw", full.names = FALSE) # Now get regions from the master file
+#regions <- regions[2:length(regions)]
+regions <- regions.todo
+
 rules <- read.csv("../cyipt/input-data/InfraSelectionRules.csv", stringsAsFactors = FALSE)
 
-#Remove Unneded Columns
-vars$X <- NULL
-vars$osm_id <- NULL
-pct$osm_id <- NULL
-pct$X <- NULL
-#width$osm_id <- NULL
-#width$X <- NULL
-
-#Join Togther
-osm <- left_join(osm,vars, by = c("id" = "id"))
-osm <- left_join(osm,pct, by = c("id" = "id"))
-#osm <- left_join(osm,width, by = c("id" = "id"))
-#osm <- left_join(osm,traffic, by = c("osm_id" = "osm_id"))
-
-rm(vars,pct,width,traffic)
-
-#Step 1: Remove Roads with low propencity to cycle
-#osm <- osm[osm$pct_census > 9,]
-
-#############################################################################################
-#Step 2: Guess Road Speed if one not provided
-summary(osm$maxspeed)
-for(a in 1:nrow(osm)){
-  if(is.na(osm$maxspeed[a])){
-    type <- osm$highway[a]
-    if(type == "motorway" | type == "motorway_link"){
-      osm$maxspeed[a] <- "70 mph"
-    }else if(type == "trunk" | type == "trunk_link"){
-      osm$maxspeed[a] <- "60 mph"
-    }else if(type == "primary" | type == "residential" | type == "road" | type == "primary_link" | type == "secondary" | type == "secondary_link" | type == "tertiary" | type == "tertiary_link"){
-      osm$maxspeed[a] <- "30 mph"
-    }else if(type == "service" ){
-      osm$maxspeed[a] <- "20 mph"
-    }else if(type == "bridleway" | type ==  "construction" | type ==  "cycleway" | type ==  "demolished" | type ==  "escalator" | type ==  "footway" | type ==  "living_street" | type ==  "steps" | type ==  "track" | type ==  "unclassified"){
-      osm$maxspeed[a] <- "10 mph"
+for(b in 1:length(regions)){
+  if(file.exists(paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-lines.Rds"))){
+    #Get file
+    osm <- readRDS(paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-lines.Rds"))
+    #Check if PCT values exist in the file
+    if(all(c("FILL ME IN") %in% names(osm)) & skip){
+      message(paste0("infrastructure types values already calcualted for ",regions[b]," so skipping"))
     }else{
-      osm$maxspeed[a] <- "60 mph"
+      message(paste0("Getting infrastructure types values for ",regions[b]," at ",Sys.time()))
+
+      #If overwriting remove old data
+      col.to.keep <- names(osm)[!names(osm) %in% c("FILL ME IN")]
+      osm <- osm[,col.to.keep]
+      rm(col.to.keep)
+
+      #Step 1: Remove Roads with low propencity to cycle
+      osm.working <- osm
+      osm.working <- osm.working[osm.working$pct.census > 9,]
+
+
+
+
+      #Save results
+      if(overwrite){
+        saveRDS(osm,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-lines.Rds"))
+      }else{
+        saveRDS(osm,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-lines-pct.Rds"))
+      }
+      rm(osm,pct.all,respar)
+
+
+
     }
-  }
-}
-summary(osm$maxspeed)
 
-osm$speed <- NA
-for(c in 1:nrow(osm)){
-  if(osm$maxspeed[c] == "70 mph" | osm$maxspeed[c] == "70" ){
-    osm$speed[c] <- 70
-  }else if(osm$maxspeed[c] == "60 mph"| osm$maxspeed[c] == "60" ){
-    osm$speed[c] <- 60
-  }else if(osm$maxspeed[c] == "50 mph"| osm$maxspeed[c] == "50" ){
-    osm$speed[c] <- 50
-  }else if(osm$maxspeed[c] == "40 mph"| osm$maxspeed[c] == "40" ){
-    osm$speed[c] <- 40
-  }else if(osm$maxspeed[c] == "30 mph"| osm$maxspeed[c] == "30" ){
-    osm$speed[c] <- 30
-  }else if(osm$maxspeed[c] == "20 mph"| osm$maxspeed[c] == "20" ){
-    osm$speed[c] <- 20
-  }else if(osm$maxspeed[c] == "10 mph"| osm$maxspeed[c] == "10" ){
-    osm$speed[c] <- 10
-  }else if(osm$maxspeed[c] == "15 mph"| osm$maxspeed[c] == "15" ){
-    osm$speed[c] <- 15
-  }else if(osm$maxspeed[c] == "5 mph"| osm$maxspeed[c] == "5" ){
-    osm$speed[c] <- 5
   }else{
-    osm$speed[c] <- 30
+    message(paste0("Input File Missing for ",regions[b]," at ",Sys.time()))
   }
 }
+rm(b,regions)
 
-rm(a,c,type)
+
+
+
 ###########################################################################
 #Step 3: Remove NA values
 osm$aadt[is.na(osm$aadt)] <- 0
