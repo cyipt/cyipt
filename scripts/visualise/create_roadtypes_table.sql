c5b0c0d49c2902271c79b45a096a8e90364e72e6
mem48 <malcolmmorgan02@hotmail.com>
2017-09-06 14:13:52 +0100

    road types table

--- scripts/visualise/create_roadtypes_table.sql
+++ scripts/visualise/create_roadtypes_table.sql
@@ -1,25 +1,13 @@
-CREATE TABLE IF NOT EXISTS bristol (
-		id INT PRIMARY KEY,
-		osmid INT NOT NULL,
-		name VARCHAR(255) NULL,
-		highway VARCHAR(255) NULL,
-		junction VARCHAR(255) NULL,
-		roadtype VARCHAR(255) NULL,
-		onewaysummary VARCHAR(255) NULL,
-		elevation VARCHAR(255) NULL,
-		maxspeed SMALLINT NULL,
-		segregated VARCHAR(255) NULL,
-		sidewalk VARCHAR(255) NULL,
-		cyclewayleft VARCHAR(255) NULL,
+CREATE TABLE IF NOT EXISTS roadtypes(
+	
+		rtid INT PRIMARY KEY,
+		roadtype VARCHAR(100) NULL,
+		onewaysummary VARCHAR(40) NULL,
+		sidewalk VARCHAR(20) NULL,
+		cyclewayleft VARCHAR(20) NULL,
 		lanespsvforward SMALLINT NULL,
 		lanesforward SMALLINT NULL,
 		lanesbackward SMALLINT NULL,
 		lanespsvbackward SMALLINT NULL,
-		cyclewayright VARCHAR(255) NULL,
-		pctcensus SMALLINT NULL,
-		pctgov INT NULL,
-		pctgen INT NULL,
-		pctdutch INT NULL,
-		pctebike INT NULL,
-		geotext GEOMETRY NOT NULL
+		cyclewayright VARCHAR(20) NULL
     );
