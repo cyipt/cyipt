ef9fb9eb8f1f8cbf763c9fac3ed455c1b783a5fd
mem48 <malcolmmorgan02@hotmail.com>
2017-11-01 17:31:35 +0000

    New paralell functions

--- R/functions.R
+++ R/functions.R
@@ -84,6 +84,126 @@ width_apply <- function(a,p){
   return(width)
 }
 
+###########################################################################
+# Based on https://www.spatialanalytics.co.nz/post/2017/09/11/a-parallel-function-for-spatial-analysis-in-r/
+
+
+# Paralise any simple features analysis.
+st_par <- function(sf_df, sf_func, n_cores, ...){
+
+  # Create a vector to split the data set up by.
+  split_vector <- rep(1:n_cores, each = nrow(sf_df) / n_cores, length.out = nrow(sf_df))
+
+  # Perform GIS analysis
+  split_results <- split(sf_df, split_vector) %>%
+    #mclapply.hack(function(x) sf_func(x, ...), mc.cores = n_cores)
+    mclapply.hack(function(x) sf_func(x, ...))
+
+  # Combine results back together. Method of combining depends on the output from the function.
+  if ( class(split_results[[1]]) == 'list' ){
+    result <- do.call("c", split_results)
+    names(result) <- NULL
+  } else {
+    result <- do.call("rbind", split_results)
+  }
+
+  # Return result
+  return(result)
+}
+
+
+
+
+
+
+########################################################################
+# from https://www.r-bloggers.com/implementing-mclapply-on-windows-a-primer-on-embarrassingly-parallel-computation-on-multicore-systems-with-r/
+# allows mclapply to run on windows
+
+##
+## mclapply.hack.R
+##
+## Nathan VanHoudnos
+## nathanvan AT northwestern FULL STOP edu
+## July 14, 2014
+##
+## A script to implement a hackish version of
+## parallel:mclapply() on Windows machines.
+## On Linux or Mac, the script has no effect
+## beyond loading the parallel library.
+
+
+## Define the hack
+mclapply.hack <- function(...) {
+  ## Create a cluster
+  size.of.list <- length(list(...)[[1]])
+  cl <- makeCluster( min(size.of.list, detectCores()) )
+
+  ## Find out the names of the loaded packages
+  loaded.package.names <- c(
+    ## Base packages
+    sessionInfo()$basePkgs,
+    ## Additional packages
+    names( sessionInfo()$otherPkgs ))
+
+  tryCatch( {
+
+    ## Copy over all of the objects within scope to
+    ## all clusters.
+    this.env <- environment()
+    while( identical( this.env, globalenv() ) == FALSE ) {
+      clusterExport(cl,
+                    ls(all.names=TRUE, env=this.env),
+                    envir=this.env)
+      this.env <- parent.env(environment())
+    }
+    clusterExport(cl,
+                  ls(all.names=TRUE, env=globalenv()),
+                  envir=globalenv())
+
+    ## Load the libraries on all the clusters
+    ## N.B. length(cl) returns the number of clusters
+    parLapply( cl, 1:length(cl), function(xx){
+      lapply(loaded.package.names, function(yy) {
+        require(yy , character.only=TRUE)})
+    })
+
+    ## Run the lapply in parallel
+    return( parLapply( cl, ...) )
+  }, finally = {
+    ## Stop the cluster
+    stopCluster(cl)
+  })
+}
+
+## Warn the user if they are using Windows
+if( Sys.info()[['sysname']] == 'Windows' ){
+  message(paste(
+    "\n",
+    "   *** Microsoft Windows detected ***\n",
+    "   \n",
+    "   For technical reasons, the MS Windows version of mclapply()\n",
+    "   is implemented as a serial function instead of a parallel\n",
+    "   function.",
+    "   \n\n",
+    "   As a quick hack, we replace this serial version of mclapply()\n",
+    "   with a wrapper to parLapply() for this R session. Please see\n\n",
+    "     http://www.stat.cmu.edu/~nmv/2014/07/14/implementing-mclapply-on-windows \n\n",
+    "   for details.\n\n"))
+}
+
+## If the OS is Windows, set mclapply to the
+## the hackish version. Otherwise, leave the
+## definition alone.
+mclapply <- switch( Sys.info()[['sysname']],
+                    Windows = {mclapply.hack},
+                    Linux   = {mclapply},
+                    Darwin  = {mclapply})
+
+## end mclapply.hack.R
+
+
+
 ####################################################################
 #OLD CODE ?
 #COnvert Multipart Lines/Ploygons to Single Parts as data frame
