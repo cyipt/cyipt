2a9b93ccc72a2a4a12dd0092af75ed62b4cdd3f6
Malcolm Morgan <mem48@users.noreply.github.com>
2017-11-20 12:58:12 +0000

    include all data and add deomographics

--- scripts/prereq_data/prep_pct.R
+++ scripts/prereq_data/prep_pct.R
@@ -1,13 +1,14 @@
-#Prepare PCT data fro use in CyIPT
+#Prepare PCT data for use in CyIPT
 #Gets all routes
 #joins in the PCT estimates
-#Removed routes ther there is never any cycling estimated
 #Saves out in sf format projected as Brit Nat Grid epsg:27700
 
 
 library(sp)
 library(sf)
 library(dplyr)
+#library(readr)
+#library(stplanr)
 
 #Get PCT data
 
@@ -26,11 +27,11 @@ pct3 <- st_as_sf(pct3)
 pct.all <- rbind(pct4,pct3)
 rm(pct3,pct4)
 
-#Get flow data
+#get flow data
 flow <- readRDS("D:/Users/earmmor/OneDrive - University of Leeds/Cycling Big Data/LSOA/LSOA_flow.Rds")
-flow$motorvehicle <- flow$carorvan + flow$motorcycle + flow$taxi + flow$passenger
-flow$publictransport <- flow$bus + flow$train + flow$underground
-flow <- flow[,c("id","all_16p","bicycle_16p","onfoot","motorvehicle","publictransport","other","is_two_way")]
+#flow$motorvehicle <- flow$carorvan + flow$motorcycle + flow$taxi + flow$passenger
+#flow$publictransport <- flow$bus + flow$train + flow$underground
+#flow <- flow[,c("id","all_16p","bicycle_16p","onfoot","motorvehicle","publictransport","other","is_two_way")]
 flow <- flow[flow$id %in% pct.all$ID,]
 flow_data <-  read.csv("D:/Users/earmmor/OneDrive - University of Leeds/Cycling Big Data/LSOA/flow_results_nat_round_170121.csv")
 names(flow_data) <- c("id","pct.gov","pct.gen","pct.dutch","pct.ebike")
@@ -45,13 +46,15 @@ rm(flow,flow_data)
 names(pct.all)[names(pct.all) == 'bicycle_16p'] <- 'pct.census'
 
 #Dump unneeded columns
-pct.all <- pct.all[,c("ID","length","busyness","av_incline","pct.census","is_two_way","pct.gov","pct.gen","pct.dutch","pct.ebike","onfoot","motorvehicle","publictransport","other","geometry")]
-class(pct.all$pct.census)
+#pct.all <- pct.all[,c("ID","length","busyness","av_incline","pct.census","is_two_way","pct.gov","pct.gen","pct.dutch","pct.ebike","onfoot","motorvehicle","publictransport","other","geometry")]
+#class(pct.all$pct.census)
 
 #find where bike is always 0
-rsum <- pct.all$pct.census + pct.all$pct.dutch + pct.all$pct.ebike +pct.all$pct.gov + pct.all$pct.gen
-pct.all <- pct.all[rsum > 0,]
-rm(rsum)
+#rsum <- pct.all$pct.census + pct.all$pct.dutch + pct.all$pct.ebike +pct.all$pct.gov + pct.all$pct.gen
+#pct.all <- pct.all[rsum > 0,]
+#rm(rsum)
 
 #save resutls
 saveRDS(pct.all,"../cyipt-securedata/pct-routes-all.Rds")
+rm(pct.all)
+gc()
