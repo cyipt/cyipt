50f6deaf5d014ab1e2d21eae51cd4d473bd1b4b1
Malcolm Morgan <mem48@users.noreply.github.com>
2017-11-22 14:27:06 +0000

    Fix bounds problem
    Fix for #59

--- scripts/prereq_data/prep_TTWA.R
+++ scripts/prereq_data/prep_TTWA.R
@@ -1,12 +1,14 @@
 # PRep the TTWAs
 library(sf)
 library(stringr)
+library(dplyr)
 
-bounds <- st_read("../cyipt-bigdata/boundaries/TTWA/shape/Travel_to_Work_Areas_December_2011_Ultra_Generalised_Clipped_Boundaries_in_United_Kingdom.shp")
+bounds <- st_read("../cyipt-bigdata/boundaries/TTWA/shape/full extent/Travel_to_Work_Areas_December_2011_Full_Extent_Boundaries_in_United_Kingdom.shp")
 bounds$ttwa11cd <- as.character(bounds$ttwa11cd)
-bounds <- bounds[substr(bounds$ttwa11cd,1,1) == "E",]
+bounds <- st_transform(bounds, 27700)
+bounds <- st_simplify(bounds, dTolerance = 10)
 
-bounds <- st_transform(bounds,4326)
+bounds <- bounds[substr(bounds$ttwa11cd,1,1) == "E" |bounds$ttwa11nm %in% c("Carlisle","Chester","Oswestry","Motherwell and Airdrie"),]
 
 bounds$ttwa11nm <- as.character(bounds$ttwa11nm)
 bounds$ttwa11nm <- str_replace_all(bounds$ttwa11nm,"[[:punct:]]","")
