ec7f8e916f1a12f26513a51047bc48b7ae4b2604
Malcolm Morgan <mem48@users.noreply.github.com>
2017-12-22 16:30:11 +0000

    split collisions into ttwas

--- scripts/prereq_data/divide_collisions.R
+++ scripts/prereq_data/divide_collisions.R
@@ -24,34 +24,25 @@ cas.all <- readRDS("../cyipt-bigdata/collisions/cas.Rds")
 veh.all <- readRDS("../cyipt-bigdata/collisions/veh.Rds")
 
 #Get Boundy
-bounds <- readRDS("../cyipt-bigdata/boundaries/local_authority/local_authority.Rds")
-
-#Subset to england
-bounds$lad16cd <- as.character(bounds$lad16cd)
-bounds <- bounds[substr(bounds$lad16cd,1,1) == "E",]
-
-#Strip symbols
-bounds$lad16nm <- as.character(bounds$lad16nm)
-bounds$lad16nm <- str_replace_all(bounds$lad16nm,"[[:punct:]]","")
-bounds$lad16nm <- str_replace_all(bounds$lad16nm," ","")
-
+bounds <- readRDS("../cyipt-bigdata/boundaries/TTWA/TTWA_England.Rds")
+bounds <- st_transform(bounds, 27700)
 
 #Loop Over each region
-for(a in 21:nrow(bounds)){
+for(a in 1:nrow(bounds)){
   region <- bounds[a,]
-  region.name <- region$lad16nm[1]
+  region.name <- region$ttwa11nm[1]
   message(paste0("Doing ",region.name," at ",Sys.time()))
   acc <- acc.all[region,]
   if(nrow(acc) == 0){
     message(paste0("No Data for ",region.name," so skipping"))
   }else{
     cas <- cas.all[cas.all$AccRefGlobal %in% acc$AccRefGlobal,]
-    foo <- sapply(1:nrow(acc),count.bike.cas)
+    #foo <- sapply(1:nrow(acc),count.bike.cas)
     acc$nCasualtiesBike <- sapply(1:nrow(acc),count.bike.cas)
-    if(!dir.exists(paste0("../cyipt-bigdata/collisions/",region.name))){
-      dir.create(paste0("../cyipt-bigdata/collisions/",region.name))
+    if(!dir.exists(paste0("../cyipt-bigdata/collisions/regions/",region.name))){
+      dir.create(paste0("../cyipt-bigdata/collisions/regions/",region.name))
     }
-    saveRDS(acc, paste0("../cyipt-bigdata/collisions/",region.name,"/collisions-summary.Rds"))
+    saveRDS(acc, paste0("../cyipt-bigdata/collisions/regions/",region.name,"/collisions-summary.Rds"))
     rm(acc,cas,region,region.name)
   }
 
