c51f8bab9e1d0d553356b4727c1621d7cbdd96ca
Malcolm Morgan <mem48@users.noreply.github.com>
2017-12-23 14:19:17 +0000

    code tidy

--- scripts/prep_data/get_collisions.R
+++ scripts/prep_data/get_collisions.R
@@ -7,6 +7,9 @@
 library(sf)
 library(dplyr)
 library(parallel)
+library(tmap)
+tmap_mode("view")
+#library(foreach)
 
 
 #Settings now come from master file
@@ -20,6 +23,7 @@ library(parallel)
 #Get the
 get.collisions <- function(a){
   #acc.sub <- acc[a,]
+  #message(paste0(Sys.time()," Doing ",a))
   acc.sub <- acc[a,c("AccRefGlobal","JunctionDetail","geometry")]
   gridid <- grid_acc[a][[1]]
 
@@ -28,7 +32,7 @@ get.collisions <- function(a){
     junc_grid <- grid_junc[gridid]
     junc_grid <- unlist(junc_grid)
     junc_grid <- junc_grid[!duplicated(junc_grid)]
-    junc_presub <- junc[junc_grid,] #select all the osm lines in the same grid cell
+    junc_presub <- junc[junc_grid,] #select all the osm junctions in the same grid cell
 
     if(nrow(junc_presub) >= 1){
       #Get distance to lines
@@ -71,25 +75,6 @@ get.collisions <- function(a){
 }
 
 
-count.collisions.lines <- function(c){
-  lineid <- osm$id[c]
-  acc.sub <- acc.lines[acc.lines$CollisionLine == lineid,]
-  nslight <- length(acc.sub$Severity[acc.sub$Severity == "Slight"])
-  nserious <- length(acc.sub$Severity[acc.sub$Severity == "Serious"])
-  nfatal <- length(acc.sub$Severity[acc.sub$Severity == "Fatal"])
-  result <- data.frame(lineid = lineid, nslight = nslight, nserious = nserious, nfatal= nfatal)
-  return(result)
-}
-
-count.collisions.junctions <- function(c){
-  juncid <- junc$osm_id[c]
-  junc.sub <- acc.junc[acc.junc$CollisionJunc == juncid,]
-  nslight <- length(junc.sub$Severity[acc.sub$Severity == "Slight"])
-  nserious <- length(junc.sub$Severity[acc.sub$Severity == "Serious"])
-  nfatal <- length(junc.sub$Severity[acc.sub$Severity == "Fatal"])
-  result <- data.frame(juncid = juncid, nslight = nslight, nserious = nserious, nfatal= nfatal)
-  return(result)
-}
 
 
 #List folders
@@ -113,10 +98,11 @@ for(b in 1:length(regions)){
       rm(col.to.keep)
 
       #Get collisions data
-      acc <- readRDS(paste0("../cyipt-bigdata/collisions/",regions[b],"/collisions-summary.Rds"))
+      acc <- readRDS(paste0("../cyipt-bigdata/collisions/regions/",regions[b],"/collisions-summary.Rds"))
 
       #Get junction locations
       junc <- readRDS(paste0("../cyipt-bigdata/osm-raw/",regions[b],"/osm-junction-points.Rds"))
+      junc$osm_id <- as.integer(junc$osm_id)
 
       #Performacne Tweak, Preallocate object to a gid to reduce processing time
       grid <- st_make_grid(osm, n = c(100,100), "polygons")
@@ -129,50 +115,61 @@ for(b in 1:length(regions)){
 
       message(paste0("Preparations complete, starting data collection at ",Sys.time()))
 
-      #For each collision find the closes road or junction
-      #doe not seem to benefit from parallisation
+      #Get the Collisions Values
+      m = 1 #Start
+      #n = 200
+      n = nrow(acc) #End
+
+
+      ##########################################################
+      #Parallel
       start <- Sys.time()
-      res <- lapply(1:nrow(acc), get.collisions)
-      res <- do.call("rbind",res)
+      fun <- function(cl){
+        parLapply(cl, m:n, get.collisions)
+      }
+      cl <- makeCluster(ncores) #make clusert and set number of cores
+      clusterEvalQ(cl, {library(sf)})
+      clusterExport(cl=cl, varlist=c("osm", "acc","junc","grid_osm","grid_acc","grid_junc"), envir=environment())
+      #clusterSplit(cl, m:n)
+      respar <- fun(cl)
+      #respar <- parLapply(cl,m:n,get.collisions)
+      stopCluster(cl)
+      respar <- bind_rows(respar)
       end <- Sys.time()
-      message(paste0("Did ",nrow(acc)," collisions in ",round(difftime(end,start,units = "secs"),2)," seconds, in serial mode at ",Sys.time()))
-      rm(start,end)
+      message(paste0("Did ",n-m + 1," collisions in ",round(difftime(end,start,units = "secs"),2)," seconds, in parallel mode at ",Sys.time()))
+      ##################################################
+      rm(n,m,cl,grid_osm,start,end)
 
       #Join togther data
       acc <- left_join(acc,respar, by = c("AccRefGlobal" = "AccRefGlobal"))
-      acc.lines <- acc[!is.na(acc$CollisionLine),]
-      acc.junc <- acc[!is.na(acc$CollisionJunc),]
-      rm(res)
+      acc.lines <- as.data.frame(acc[!is.na(acc$CollisionLine),])
+      acc.lines$geometry <- NULL
+      acc.junc <- as.data.frame(acc[!is.na(acc$CollisionJunc),])
+      acc.lines$geometry <- NULL
 
       saveRDS(acc,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/collisions.Rds"))
 
-      m = 1
-      n = nrow(osm)
+      acc.lines <- acc.lines %>%
+        group_by(CollisionLine) %>%
+        summarise(ncollisions = length(AccRefGlobal), bikeCas = sum(nCasualtiesBike), totalCas = sum(nCasualties), totalVeh = sum(nVehicles))
 
-      start <- Sys.time()
-      res <- lapply(1:nrow(osm), count.collisions.lines)
-      end <- Sys.time()
-      message(round(difftime(end,start,units = "secs"),2))
-      res <- do.call("rbind",res)
-      osm <- left_join(osm,res, by = c("id" = "lineid"))
-      rm(res)
-      #count.collisions.lines(1)
+      acc.junc <- acc.junc %>%
+        group_by(CollisionJunc) %>%
+        summarise(ncollisions = length(AccRefGlobal), bikeCas = sum(nCasualtiesBike), totalCas = sum(nCasualties), totalVeh = sum(nVehicles))
 
-      res <- lapply(1:nrow(junc), count.collisions.junctions)
-      res <- do.call("rbind",res)
-      acc <- left_join(acc,res, by = c("osm_id" = "juncid"))
-      rm(res)
-
-      saveRDS(acc,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/collisions.Rds"))
+      osm <- left_join(osm,acc.lines, by = c("id" = "CollisionLine"))
+      junc <- left_join(junc,acc.junc, by = c("osm_id" = "CollisionJunc"))
 
 
       #Save results
       if(overwrite){
         saveRDS(osm,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-lines.Rds"))
+        saveRDS(junc,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-junctions.Rds"))
       }else{
-        saveRDS(osm,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-lines-collisions.Rds"))
+        saveRDS(osm,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-lines-colisions.Rds"))
+        saveRDS(junc,paste0("../cyipt-bigdata/osm-prep/",regions[b],"/osm-junctions.Rds"))
       }
-      rm(osm,pct.all,respar)
+      rm(osm,acc.junc,acc.lines,junc,respar,acc)
 
 
 
