d5f0103be36e17f77564c8bebaa81d2ae4ea4ea4
Malcolm Morgan <mem48@users.noreply.github.com>
2018-01-18 19:03:22 +0000

    remove junk code

--- scripts/prep_data/shape2rds.R
+++
@@ -1,7 +0,0 @@
-#Shapefile to R
-
-library(sf)
-shp <- st_read("../cyipt-bigdata/boundaries/local_authority/Local_Authority_Districts_December_2016_Generalised_Clipped_Boundaries_in_Great_Britain.shp")
-shp <- st_transform(shp, 27700)
-saveRDS(shp,"../cyipt-bigdata/boundaries/local_authority/local_authority.Rds")
-
