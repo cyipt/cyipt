19928366ff0d037f8534a0d271b0a698c922098d
Malcolm Morgan <mem48@users.noreply.github.com>
2018-01-18 21:02:49 +0000

    upp number of cores

--- scripts/cyipt.R
+++ scripts/cyipt.R
@@ -10,11 +10,11 @@
 #Regions are selected using the file  ../cyipt/input-data/RegionsToDo.csv
 #To do a region just put y in the do column of this csv file
 
-skip <- FALSE #Should the code skip regions that have already been done?
+skip <- TRUE #Should the code skip regions that have already been done?
 overwrite <- TRUE #Some stages overwrite existing files, for example by adding an extra column of data
                    #Note that not overwriting may cause later stagest to fail if they expect earlier stages
                    #resutls to be in the starting file
-ncores <- 4 #Some functions use parallel processing how many clusters should be run?
+ncores <- 6 #Some functions use parallel processing how many clusters should be run?
 verbose <- TRUE #Get extra messages and information
 
 
