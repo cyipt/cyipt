dd588bab5cb7e1513d4e2e96122ab919c9820731
Malcolm Morgan <mem48@users.noreply.github.com>
2018-01-29 15:29:24 +0000

    new PCT table
    With circuity support

--- scripts/visualise/create_pct.sql
+++ scripts/visualise/create_pct.sql
@@ -4,15 +4,32 @@ DROP INDEX IF EXISTS pctlsoa_gindx;
 CREATE TABLE IF NOT EXISTS pctlsoa (
 		idn INT PRIMARY KEY,
 		id VARCHAR(19) NOT NULL,
-		total INT NOT NULL,
-		cyclists INT NOT NULL,
-		drivers INT NOT NULL,
-		hilliness REAL NULL,
-		geotext GEOMETRY NOT NULL
+		allCommuters INT NULL,
+		onfoot INT NULL,
+		carorvan INT NULL,
+		pctcensus INT NULL,
+		pctgov INT NULL,
+		pctgen INT NULL,
+		pctdutch INT NULL,
+		pctebike INT NULL,
+		fastDist REAL NULL,
+		fastTime REAL NULL,
+		fastSlope REAL NULL,
+		fastGeom GEOMETRY NULL,
+		quietDist REAL NULL,
+		quietSlope REAL NULL,
+		quietTime REAL NULL,
+		quietGeom GEOMETRY NULL,
+		straightDist REAL NULL,
+		straightGeom GEOMETRY NULL,
+		fastCircuity REAL NULL,
+		quietCircuity REAL NULL,
+		quietDiversion REAL NULL,
+		quietDiverTime REAL NULL
     );
 
 COPY pctlsoa FROM '/home/malcolm/pct.csv'  csv HEADER;
 
 ALTER TABLE pctlsoa OWNER to cyipt;
 
-CREATE INDEX pctlsoa_gindx ON pctlsoa USING GIST (geotext);
+CREATE INDEX pctlsoa_gindx ON pctlsoa USING GIST (fastGeom);
--- scripts/visualise/export_pct.R
+++ scripts/visualise/export_pct.R
@@ -1,35 +1,86 @@
 # Export Regions for Database
 library(sf)
 library(tmap)
+library(dplyr)
 tmap_mode("view")
 
 
 # pcts
 
-pct <- readRDS("../cyipt-securedata/pct-routes-all.Rds")
-pct <- pct[,c("ID","all_16p","pct.census","carorvan","av_incline")]
+rf <- readRDS("../cyipt-securedata/pct-routes-all.Rds")
+rq <- readRDS("../cyipt-securedata/pct-routesquiet-all.Rds")
+l <- readRDS("../cyipt-securedata/pct-lines-all.Rds")
 
-names(pct) <- c("id","total","cyclists","drivers","hilliness","geometry") #Remove punctuation from names for POSTGIS
+#Select Relevant Columns
+rf <- rf[,c("ID","all_16p","onfoot","carorvan","pct.census","pct.gov","pct.gen","pct.dutch","pct.ebike","length","time","av_incline","geometry")]
+rq <- rq[,c("id","rq_dist_km","rq_avslope_perc","rq_time_min","geometry")]
 
+#rename columms
+names(rf) <- c("id","all","onfoot","carorvan","pctcensus","pctgov","pctgen","pctdutch","pctebike","fastDist","fastTime","fastSlope","geometry")
+names(rq) <- c("id","quietDist","quietSlope","quietTime","geometry")
+names(l) <- c("id","length","geometry")
 
-pct <- pct[pct$total > 2,]
-pct <- st_simplify(pct, dTolerance = 10) # 5.3GB unsimplified
-pct <- st_transform(pct, 4326)
+#Add Variaibles
+l$straightDist <- l$length / 1000
+l$length <- NULL
+rf$fastDist <- rf$fastDist / 1000
 
+#FIlter Out Low Numbers for Privacy
+rf <- rf[rf$all > 2,]
+rq <- rq[rq$id %in% rf$id, ]
+l <- l[l$id %in% rf$id, ]
 
-#Reduce precison of data to reduce file size
-pct$geometry <- st_as_binary(pct$geometry, precision = 100000)
-pct$geometry <- st_as_sfc(pct$geometry)
+#Some Missing Quiet Routes?
 
-qtm(pct[1:10,]) # Check the resutls are ok
+#Simplify Geometry
+rf <- st_simplify(rf, dTolerance = 10)
+rq <- st_simplify(rq, dTolerance = 10)
+
+#Transform to WSG84
+rf <- st_transform(rf, 4326)
+rq <- st_transform(rq, 4326)
+l <- st_transform(l, 4326)
 
 #convert to well known text
-pct$geotext <- st_as_text(pct$geometry)
-pct <- as.data.frame(pct)
-pct$geometry <- NULL
+rf$geometry <- st_as_text(rf$geometry, digits = 5)
+rf <- as.data.frame(rf)
 
+rq$geometry <- st_as_text(rq$geometry, digits = 5)
+rq <- as.data.frame(rq)
+
+l$geometry <- st_as_text(l$geometry, digits = 5)
+l <- as.data.frame(l)
+
+#Rename Columns
+rf$fastGeom <- rf$geometry
+rf$geometry <- NULL
+rq$quietGeom <- rq$geometry
+rq$geometry <- NULL
+l$straightGeom <- l$geometry
+l$geometry <- NULL
+
+#Join Togther
+pct <- left_join(rf,rq, by = c("id" = "id"))
+pct <- left_join(pct,l, by = c("id" = "id"))
+
+pct$fastCircuity <- pct$fastDist / pct$straightDist
+pct$quietCircuity <- pct$quietDist / pct$straightDist
+pct$quietDiversion <- pct$quietDist / pct$fastDist
+pct$quietDiverTime <- pct$quietTime / pct$fastTime
+
+# add ID column
 pct$idn <- 1:nrow(pct)
 
-pct <- pct[,c("idn","id","total","cyclists","drivers","hilliness","geotext")]
+# rearrange column
+pct <- pct[,c("idn","id","all","onfoot","carorvan","pctcensus","pctgov","pctgen","pctdutch","pctebike",
+              "fastDist","fastTime","fastSlope","fastGeom","quietDist","quietSlope","quietTime","quietGeom","straightDist",
+              "straightGeom","fastCircuity","quietCircuity","quietDiversion","quietDiverTime")]
+
+#Change names for DB
+names(pct) <- c("idn","id","allCommuters","onfoot","carorvan","pctcensus","pctgov","pctgen","pctdutch","pctebike",
+                "fastDist","fastTime","fastSlope","fastGeom","quietDist","quietSlope","quietTime","quietGeom","straightDist",
+                "straightGeom","fastCircuity","quietCircuity","quietDiversion","quietDiverTime")
+
+
 
 write.csv(pct,"../cyipt-bigdata/forDB/pct.csv", row.names = F, na = "")
