1dbdbaa53dfbb1ee49142f75623d8f4ff41910b0
Malcolm Morgan <mem48@users.noreply.github.com>
2018-02-02 15:01:34 +0000

    Clean up historic data

--- scripts/test_code/uptake/uptake_2001_2011.R
+++ scripts/test_code/uptake/uptake_2001_2011.R
@@ -82,7 +82,7 @@ if(file.exists("../cyipt-securedata/uptakemodel/rf_buff.Rds")){
 }
 
 #################################
-# Step 5: get the historic infrastrucutre data
+# Step 4: get the historic infrastrucutre data
 if(file.exists("../cyipt-securedata/uptakemodel/infra_change.Rds")){
   infra.change <- readRDS("../cyipt-securedata/uptakemodel/infra_change.Rds")
 }else{
@@ -105,7 +105,7 @@ if(file.exists("../cyipt-securedata/uptakemodel/infra_change.Rds")){
 }
 
 ###################################
-# Step 6: Get the Transport Direct Data
+# Step 5: Get the Transport Direct Data
 if(file.exists("../cyipt-securedata/uptakemodel/infra_td.Rds")){
   infra.td <- readRDS("../cyipt-securedata/uptakemodel/infra_td.Rds")
 }else{
@@ -120,23 +120,166 @@ if(file.exists("../cyipt-securedata/uptakemodel/infra_td.Rds")){
 }
 
 ###################################
-# Step 4: Find the OSM roads that intersect the buffered routes
+# Step 6: Find the OSM roads that intersect the buffered routes
 if(file.exists("../cyipt-securedata/uptakemodel/osm_rf_inter.Rds")){
   inter <- readRDS("../cyipt-securedata/uptakemodel/osm_rf_inter.Rds")
 }else{
-  inter <- st_parallel(sf_df = rf_buff, sf_func = st_intersects, n_cores = 6, y = osm)
+  inter <- st_parallel(sf_df = rf_buff, sf_func = st_intersects, n_cores = 2, y = osm[,"id"])
   saveRDS(inter,"../cyipt-securedata/uptakemodel/osm_rf_inter.Rds")
 }
 
 
 
+#################################
+# Stetp 7: Combine the two hsitoric datasets
+if(file.exists("../cyipt-securedata/uptakemodel/infra_historic.Rds")){
+  infra.historic <- readRDS("../cyipt-securedata/uptakemodel/infra_historic.Rds")
+}else{
+
+  infra.change.buff <- st_buffer(infra.change, dist = 5, nQuadSegs = 2)
+
+  infra.historic = st_join(infra.td, infra.change.buff, join = st_intersects, left = TRUE, largest = FALSE)
+  #infra.historic.old <- infra.historic
+  #clena up the tags
+  names(infra.historic)
+
+  #Summarise if on a cycle network
+  infra.historic$rcn <- as.character(infra.historic$rcn)
+  infra.historic$lcn <- as.character(infra.historic$lcn)
+  infra.historic$ncn <- as.character(infra.historic$ncn)
+
+  infra.historic$rcn[is.na(infra.historic$rcn)] <- "no"
+  infra.historic$lcn[is.na(infra.historic$lcn)] <- "no"
+  infra.historic$ncn[is.na(infra.historic$ncn)] <- "no"
+
+  infra.historic$network <- ifelse(infra.historic$rcn == "yes" | infra.historic$lcn == "yes" | infra.historic$ncn == "yes",
+                                   TRUE, FALSE)
+
+  #Clean cycle infrastrucutre
+  infra.historic$cycleway <- as.character(infra.historic$cycleway)
+  infra.historic$cycleway[infra.historic$cycleway %in% c("Lane","yes")] <- "lane"
+
+
+  infra.historic$cycleway.left.width <- as.numeric(as.character(infra.historic$cycleway.left.width))
+  infra.historic$cycleway.oneside.width <-  as.numeric(as.character(infra.historic$cycleway.oneside.width))
+  infra.historic$cycleway.otherside.width <- as.numeric(as.character(infra.historic$cycleway.otherside.width))
+  infra.historic$cycleway.right.width <- as.numeric(as.character(infra.historic$cycleway.right.width))
+
+  for(i in 1:nrow(infra.historic)){
+    left <- infra.historic$cycleway.left.width[i]
+    oneside <- infra.historic$cycleway.oneside.width[i]
+    otherside <- infra.historic$cycleway.otherside.width[i]
+    right <- infra.historic$cycleway.right.width[i]
+    if(is.na(otherside) | is.na(oneside)){
+      #no data do nothing
+    }else{
+      message(paste0("Line ",i," ",left," ",oneside," ",otherside," ",right))
+      #dat so do something
+      if(!is.na(oneside) & is.na(left)){
+        infra.historic$cycleway.left.width[i] <- oneside
+        infra.historic$cycleway.oneside.width[i] <- NA
+      }
+
+      if(!is.na(otherside) & is.na(right)){
+        infra.historic$cycleway.right.width[i] <- otherside
+        infra.historic$cycleway.otherside.width[i] <- NA
+      }
+    }
+
+
+  }
+
+  infra.historic$cycleway.left <- as.character(infra.historic$cycleway.left)
+  infra.historic$cycleway.oneside <-  as.character(infra.historic$cycleway.oneside)
+  infra.historic$cycleway.otherside <- as.character(infra.historic$cycleway.otherside)
+  infra.historic$cycleway.right <- as.character(infra.historic$cycleway.right)
+
+  infra.historic$cycleway.left[infra.historic$cycleway.left %in% c("Lane","yes")] <- "lane"
+  infra.historic$cycleway.oneside[infra.historic$cycleway.oneside %in% c("Lane","yes")] <- "lane"
+  infra.historic$cycleway.otherside[infra.historic$cycleway.otherside %in% c("Lane","yes")] <- "lane"
+  infra.historic$cycleway.right[infra.historic$cycleway.right %in% c("Lane","yes")] <- "lane"
+  infra.historic$cycleway.right[infra.historic$cycleway.right %in% c("20111212","Wide bus >4.2m")] <- NA
+
+  for(i in 1:nrow(infra.historic)){
+    left <- infra.historic$cycleway.left[i]
+    oneside <- infra.historic$cycleway.oneside[i]
+    otherside <- infra.historic$cycleway.otherside[i]
+    right <- infra.historic$cycleway.right[i]
+    if(is.na(otherside) | is.na(oneside)){
+      #no data do nothing
+    }else{
+      message(paste0("Line ",i," ",left," ",oneside," ",otherside," ",right))
+      #dat so do something
+      if(!is.na(oneside) & is.na(left)){
+        infra.historic$cycleway.left[i] <- oneside
+        infra.historic$cycleway.oneside[i] <- NA
+      }
+
+      if(!is.na(otherside) & is.na(right)){
+        infra.historic$cycleway.right[i] <- otherside
+        infra.historic$cycleway.otherside[i] <- NA
+      }
+    }
+
+
+  }
+
+  #Clean up dates
+
+  date_switch = function(d){
+    d = sapply(d, switch,
+               "2004/5" = "2005-01-01",
+               "2005/6" = "2006-01-01",
+               "2006/7" = "2007-01-01",
+               "2007/8" = "2008-01-01",
+               "2008/9" = "2009-01-01",
+               "2009/2010" = "2010-01-01",
+               "2010/2011" = "2011-01-01",
+               NA
+    )
+    as.Date(unlist(d))
+  }
+
+  infra.historic$BuildYear <- date_switch(infra.historic$BuildYear)
+  infra.historic$date <- ifelse(is.na(infra.historic$BuildYear),as.character(infra.historic$OpenDate),as.character(infra.historic$BuildYear))
+  infra.historic$date <- as.Date(infra.historic$date)
+
+
+
+  #remove unneed columns
+
+  infra.historic <- infra.historic[,!(names(infra.historic) %in% c("ncn","ncn_ref","rcn","rcn_ref","lcn","lcn_ref",
+                                                                   "ncn_name","rcn_name","lcn_name","bicycle","layer",
+                                                                   "railway","tunnel","horse","name","ref","barrier",
+                                                                   "ford","leisure","depth","ford","cycle","RouteType",
+                                                                   "bridge","motorcar","cycleway.otherside","crossing",
+                                                                   "cycleway.otherside.width","cycleway.oneside",
+                                                                   "cycleway.oneside.width","step_count","BuildYear",
+                                                                   "OpenDate","CycleRoute"))]
+
+  saveRDS(infra.historic,"../cyipt-securedata/uptakemodel/infra_historic.Rds")
+}
+
+
+
+
+
+
+
 ####################################
 # Sanity Check
-qtm(infra.td[1:1000,], lines.lwd = 2, lines.col = "red" ) +
+qtm(infra.historic[!is.na(infra.historic$BuildYear) | !is.na(infra.historic$OpenDate),], lines.lwd = 2, lines.col = "red" ) +
   qtm(infra.change, lines.lwd = 1, lines.col = "blue")
 
+
+
+
+
+
+
+
 ###################################
-# Step 7: Subset the OSM to only the parts that intersect with the routes
+# Step 9: Subset the OSM to only the parts that intersect with the routes
 
 #list the osmids
 foo <- inter[1:5]
