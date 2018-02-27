247a24b5375c7800139b56deede918341bd21ba3
Malcolm Morgan <mem48@users.noreply.github.com>
2018-02-27 10:09:52 +0000

    creat data for uptake model

--- scripts/test_code/uptake/uptake_2001_2011.R
+++ scripts/test_code/uptake/uptake_2001_2011.R
@@ -289,7 +289,8 @@ inter.unique <- inter.unique[order(inter.unique)]
 osm.onroutes <- osm[inter.unique,]
 #bounds <- readRDS("../cyipt-bigdata/osm-raw/Bristol/bounds.Rds")
 
-infra.historic.changed <- infra.historic[!is.na(infra.historic$date),]
+#infra.historic.changed <- infra.historic[!is.na(infra.historic$date),]
+infra.historic.changed <- infra.historic # keep so we can find the post 2011 infra and ignore
 #summary(is.na(infra.historic$date))
 
 
@@ -338,6 +339,320 @@ inter2 <- st_parallel(sf_df = rf_buff, sf_func = st_contains, n_cores = 4, y = o
 saveRDS(inter2,"../cyipt-securedata/uptakemodel/osm_onroutes_rf_inter.Rds")
 names(osm.final)
 
+# recorder the routes
+rf01_11 <- rf01_11[order(match(rf01_11$id,rf_buff$id)),]
+summary(rf01_11$id == rf_buff$id)
+
+rf01_11$osmids <- inter2
+saveRDS(rf01_11,"../cyipt-securedata/uptakemodel/routes01_11_withlookup.Rds")
+
+rf01_11 <- readRDS("../cyipt-securedata/uptakemodel/routes01_11_withlookup.Rds")
+
+summary(rf01_11$id == rf_buff$id)
+
+
+#summarise the osm data
+names(osm.final)
+osm.final$id.y <- as.character(osm.final$id.y)
+
+
+check.changestatus <- function(i){
+  tmp_date <- osm.final$date[i]
+  tmp_idy <- osm.final$id.y[i]
+  if(is.na(tmp_date) & is.na(tmp_idy)){
+    #Only Appears in the 2018 OSM data
+    result <- "Only in OSM"
+  }else if(is.na(tmp_date) & !is.na(tmp_idy)){
+    #In the 2011 CYcling INfrastrucutre but has no change date
+    result <- "Exists in 2011 unknown date"
+  }else if(!is.na(tmp_date) & !is.na(tmp_idy)){
+    #Have change information
+    if(tmp_date >= as.Date("2001-01-01") & tmp_date <= as.Date("2011-12-31")){
+      result <- "Built between 2001 and 2011"
+    }else if(tmp_date < as.Date("2001-01-01")){
+      result <- "Built before 2001"
+    }else if(tmp_date > as.Date("2011-12-31")){
+      result <- "Built after 2011"
+    }else{
+      result <- "Error Type 1"
+    }
+  }else{
+    result <- "Error Type 2"
+  }
+
+  return(result)
+}
+
+osm.final$changestatus <- sapply(1:nrow(osm.final), check.changestatus)
+
+# get the infrastrucutre before and after
+osm.final$highway_01 <- NA
+osm.final$highway_11 <- NA
+osm.final$highway.x <- as.character(osm.final$highway.x)
+osm.final$highway.y <- as.character(osm.final$highway.y)
+
+for(i in 1:nrow(osm.final)){
+  highwayX <- osm.final$highway.x[i]
+  highwayY <- osm.final$highway.y[i]
+  change <- osm.final$changestatus[i]
+
+  if(change == "Only in OSM"){
+    highway_01 <- highwayX
+    highway_11 <- highwayX
+  }else if(change == "Built after 2011"){
+    highway_01 <- highwayX
+    highway_11 <- highwayX
+  }else if(change == "Built before 2001"){
+    highway_01 <- highwayY
+    highway_11 <- highwayY
+  }else if(change == "Built between 2001 and 2011"){
+    highway_01 <- highwayX
+    highway_11 <- highwayY
+  }else if(change == "Exists in 2011 unknown date"){
+    highway_01 <- highwayY
+    highway_11 <- highwayY
+  }else{
+    message(paste0("Error on ",i))
+  }
+  osm.final$highway_01[i] <- highway_01
+  osm.final$highway_11[i] <- highway_11
+
+  if(i %% 1000 == 0){
+    message(paste0("Done ",i))
+  }
+  rm(highway_01,highway_11)
+}
+
+osm.final$highway_01[is.na(osm.final$highway_01)] <- osm.final$highway.x[is.na(osm.final$highway_01)]
+osm.final$highway_11[is.na(osm.final$highway_11)] <- osm.final$highway.x[is.na(osm.final$highway_11)]
+
+osm.final$maxspeed_01 <- NA
+osm.final$maxspeed_11 <- NA
+osm.final$highway.x <- as.character(osm.final$highway.x)
+
+#add on a maxspeed for the infrastrucutre change
+for(i in 1:nrow(osm.final)){
+  highwayX <- osm.final$highway.x[i]
+  highway_01 <- osm.final$highway_01[i]
+  highway_11 <- osm.final$highway_11[i]
+  maxspeedX <- osm.final$maxspeed[i]
+  #change <- osm.final$changestatus[i]
+
+  if(highway_01 == highwayX){
+    maxspeed_01 <- maxspeedX
+  }else{
+    if(highway_01 %in% c("motorway","motorway_link")){
+      maxspeed_01 <- 70
+    }else if(highway_01 %in% c("trunk","trunk_link","bus_guideway")) {
+      maxspeed_01 <- 60
+    }else if(highway_01 %in% c("primary","residential","road","primary_link","secondary","secondary_link","tertiary","tertiary_link")){
+      maxspeed_01 <- 30
+    }else if(highway_01 == "service" ){
+      maxspeed_01 <- 20
+    }else if(highway_01 %in% c("path","bridleway","construction","cycleway","demolished","escalator","footway","living_street","steps","track","unclassified","pedestrian")){
+      maxspeed_01 <- 10
+    }else{
+      maxspeed_01 <- 60
+    }
+  }
+
+  if(highway_11 == highwayX){
+    maxspeed_11 <- maxspeedX
+  }else{
+    if(highway_01 %in% c("motorway","motorway_link")){
+      maxspeed_11 <- 70
+    }else if(highway_01 %in% c("trunk","trunk_link","bus_guideway")) {
+      maxspeed_11 <- 60
+    }else if(highway_01 %in% c("primary","residential","road","primary_link","secondary","secondary_link","tertiary","tertiary_link")){
+      maxspeed_11 <- 30
+    }else if(highway_01 == "service" ){
+      maxspeed_11 <- 20
+    }else if(highway_01 %in% c("path","bridleway","construction","cycleway","demolished","escalator","footway","living_street","steps","track","unclassified","pedestrian")){
+      maxspeed_11 <- 10
+    }else{
+      maxspeed_11 <- 60
+    }
+  }
+  if(i %% 10000 == 0){
+    message(paste0(Sys.time(),": Done ",i))
+  }
+
+  osm.final$maxspeed_11[i] <- maxspeed_11
+  osm.final$maxspeed_01[i] <- maxspeed_01
+}
+
+saveRDS(osm.final,"../cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+saveRDS(osm.final,"N:/Earth&Environment/Research/ITS/Research-1/CyIPT/cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+
+# summarise the cycleway
+
+osm.final$cycleway_osm <- NA
+
+for(i in 1:nrow(osm.final)){
+  left <- osm.final$cycleway.left.x[i]
+  right <- osm.final$cycleway.right.x[i]
+
+  if(left == right ){
+    result <- left
+  }else if(left == "no"){
+    result <- right
+  }else if(right == "no"){
+    result <- left
+  }else if(right %in% c("share_busway","lane") & left %in% c("lane","track") ){
+    result <- left
+  }else if(left %in% c("share_busway","lane") & right %in% c("lane","track") ){
+    result <- right
+  }else{
+    message(paste0("Unusual case for row ",i," left = ",left," right = ",right))
+    result <- left
+  }
+  osm.final$cycleway_osm[i] <- result
+}
+
+names(osm.final)
+
+saveRDS(osm.final,"../cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+saveRDS(osm.final,"N:/Earth&Environment/Research/ITS/Research-1/CyIPT/cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+
+#summarise other cycleway data
+
+osm.final$cycleway_change <- NA
+osm.final$cycleway.right.y[is.na(osm.final$cycleway.right.y)] <- "no"
+osm.final$cycleway.left.y[is.na(osm.final$cycleway.left.y)] <- "no"
+
+for(i in 1:nrow(osm.final)){
+  left <- osm.final$cycleway.left.y[i]
+  right <- osm.final$cycleway.right.y[i]
+
+  if(left == right ){
+    result <- left
+  }else if(left == "no"){
+    result <- right
+  }else if(right == "no"){
+    result <- left
+  }else{
+    message(paste0("Unusual case for row ",i," left = ",left," right = ",right))
+    result <- left
+  }
+  osm.final$cycleway_change[i] <- result
+}
+
+saveRDS(osm.final,"../cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+saveRDS(osm.final,"N:/Earth&Environment/Research/ITS/Research-1/CyIPT/cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+
+# Approximage the 2001 and 2011 infrastrucutre
+
+osm.final$cycleway_01 <- NA
+
+for(i in 1:nrow(osm.final)){
+  change <- osm.final$changestatus[i]
+  cycleway_change <- osm.final$cycleway_change[i]
+  cycleway_osm <- osm.final$cycleway_osm[i]
+
+  if(change %in% c("Built after 2011", "Built between 2001 and 2011")){
+    result <- "no"
+  }else if(change %in% c("Built before 2001")){
+    result <- cycleway_change
+  }else if(change %in% c("Exists in 2011 unknown date")){
+    result <- cycleway_change
+  }else if(change %in% c("Only in OSM")){
+    result <- cycleway_osm
+  }else{
+    message(paste0("error on line ",i))
+  }
+
+  osm.final$cycleway_01[i] <- result
+}
+
+
+osm.final$cycleway_11 <- NA
+
+for(i in 1:nrow(osm.final)){
+  change <- osm.final$changestatus[i]
+  cycleway_change <- osm.final$cycleway_change[i]
+  cycleway_osm <- osm.final$cycleway_osm[i]
+
+  if(change %in% c("Built after 2011")){
+    result <- "no"
+  }else if(change %in% c("Built before 2001","Built between 2001 and 2011")){
+    result <- cycleway_change
+  }else if(change %in% c("Exists in 2011 unknown date")){
+    result <- cycleway_change
+  }else if(change %in% c("Only in OSM")){
+    result <- cycleway_osm
+  }else{
+    message(paste0("error on line ",i))
+  }
+
+  osm.final$cycleway_11[i] <- result
+}
+
+osm.final$length <- as.numeric(st_length(osm.final))
+
+saveRDS(osm.final,"../cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+saveRDS(osm.final,"N:/Earth&Environment/Research/ITS/Research-1/CyIPT/cyipt-securedata/uptakemodel/osm_onroutes_withchange_0111.Rds")
+
+#Clean the Highway Tags
+
+osm.final$highway_01 <- sub("_link","",osm.final$highway_01)
+osm.final$highway_11 <- sub("_link","",osm.final$highway_11)
+
+osm.final$highway_01[osm.final$highway_01 %in% c("track", "pedestrian","steps","bridleway","byway", "footway")] <- "path"
+osm.final$highway_11[osm.final$highway_11 %in% c("track", "pedestrian","steps","bridleway","byway", "footway")] <- "path"
+
+osm.final$highway_01[osm.final$highway_01 %in% c("unclassified", "service","living_street","road","byway", "Other","bus_guideway","BOAT")] <- "other"
+osm.final$highway_11[osm.final$highway_11 %in% c("unclassified", "service","living_street","road","byway", "Other","bus_guideway","BOAT")] <- "other"
+
+
+#summarise Infrastrucutre change for each route
+
+get.infrachange <- function(x){
+  route <- rf01_11[x,]
+  osm_sub <- osm.final[route$osmids[[1]],]
+  osm_sub <- as.data.frame(osm_sub)
+  osm_sub <- osm_sub[,c("highway_01","highway_11","maxspeed_01","maxspeed_11","cycleway_01","cycleway_11","length")]
+
+  osm_summary01 <- osm_sub[,c("highway_01","maxspeed_01","cycleway_01","length")]
+  osm_summary01 <- group_by(osm_summary01, highway_01,maxspeed_01,cycleway_01)
+  osm_summary01 <- summarise(osm_summary01, length = sum(length))
+
+  osm_summary11 <- osm_sub[,c("highway_11","maxspeed_11","cycleway_11","length")]
+  osm_summary11 <- group_by(osm_summary11, highway_11,maxspeed_11,cycleway_11)
+  osm_summary11 <- summarise(osm_summary11, length = sum(length))
+
+  result <- list()
+  result[[1]] <- as.data.frame(osm_summary01)
+  result[[2]] <- as.data.frame(osm_summary11)
+  attr(result,'routeID') <- route$id[1]
+  return(result)
+}
+
+#foo <- lapply(1:10, get.infrachange)
+
+##########################################################
+#Parallel
+m = 1 #Start
+n = nrow(osm.final) #End
+
+start <- Sys.time()
+fun <- function(cl){
+  parLapply(cl, m:n, get.infrachange)
+}
+cl <- makeCluster(5) #make clusert and set number of cores
+clusterEvalQ(cl, {library(sf); library(dplyr)})
+clusterExport(cl=cl, varlist=c("osm.final", "rf01_11"), envir=environment())
+respar <- fun(cl)
+stopCluster(cl)
+#respar <- bind_rows(respar)
+end <- Sys.time()
+message(paste0("Did ",n-m + 1," routes in ",round(difftime(end,start,units = "secs"),2)," seconds, in parallel mode at ",Sys.time()))
+rm(n,m,cl,start,end,fun)
+##################################################
+
+saveRDS(respar,"../cyipt-securedata/uptakemodel/route_infra_BeforeAfter.Rds")
+saveRDS(respar,"N:/Earth&Environment/Research/ITS/Research-1/CyIPT/cyipt-securedata/uptakemodel/route_infra_BeforeAfter.Rds")
+
+
 
 #qtm(osm.final[1:10000,], lines.col = "highway.y", lines.lwd = 5) +
 #  qtm(infra.historic.onroute, lines.lwd = 1, lines.col = "black")
@@ -348,9 +663,7 @@ names(osm.final)
 
 #rf01_11 <- readRDS("../cyipt-securedata/uptakemodel/routes01_11.Rds")
 #summary(rf01_11$id == rf_buff$id)
-# recorder the routes
-#rf01_11 <- rf01_11[order(match(rf01_11$id,rf_buff$id)),]
-#summary(rf01_11$id == rf_buff$id)
+
 #rf01_11$osm_ids <- inter
 #rm(rf_buff)
 
