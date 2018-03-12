758ca5d84d9a970186820b8c3e7a0683fa096081
robinlovelace <rob00x@gmail.com>
2018-03-12 14:34:00 +0000

    Update model with new vars

--- scripts/test_code/uptake/uptake_2001_2011_results.R
+++ scripts/test_code/uptake/uptake_2001_2011_results.R
@@ -4,15 +4,14 @@ library(sf)
 min_cycle = 0
 min_all = 20
 
+# load and preprocess data ----
 rf_sp <- readRDS("N:/Earth&Environment/Research/ITS/Research-1/CyIPT/cyoddata/rf.Rds")
-rf_sf <- st_as_sf(rf)
+rf_sf <- st_as_sf(rf_sp)
 rf <- as.data.frame(rf_sf)
 rf <- rf[,c("id","rf_avslope_perc","rf_time_min","e_dist_km")]
 head(rf)
-
 routes <- readRDS("../cyipt-securedata/uptakemodel/route_infra_final2.Rds")
 routes <- left_join(routes, rf, by = c("id" = "id"))
-
 routes$all01[is.na(routes$all01)] <- 0
 routes$bicycle01[is.na(routes$bicycle01)] <- 0
 routes$percycle01 <- routes$bicycle01 / routes$all01
@@ -20,7 +19,70 @@ routes$percycle11 <- routes$bicycle11 / routes$all11
 routes$changecommuters <- (routes$all11 - routes$all01) / routes$all01
 routes$Puptake <- routes$percycle11 - routes$percycle01
 routes[is.na(routes)] <- 0
-routes2 = routes
+
+# total length of changed infra:
+routes_infra_change = routes %>%
+  select(matches("C", ignore.case = F)) %>%
+  select(-contains("N", ignore.case = F))
+summary(routes_infra_change)
+routes_infra_change_pos = mutate_all(routes_infra_change, .funs = funs(ifelse(. < 0, 0, .)))
+summary(routes_infra_change_pos)
+routes_infraC = rowSums(routes_infra_change_pos)
+summary(routes_infraC)
+routes$routes_infra_length = routes_infraC
+cor(routes$Puptake, routes$routes_infra_length) # negative correlation (unexpected)
+
+# find percentage on roads with different speeds:
+# 20- mph
+routes_pspeeds20 = routes %>%
+  select(matches("20")) %>%
+  select(contains("P"), -contains("01"))
+summary(routes_pspeeds20)
+routes_pspeed20 = rowSums(routes_pspeeds20)
+summary(routes_pspeed20)
+routes$routes_pspeed20 = routes_pspeed20
+# 30 mph
+routes_pspeeds30 = routes %>%
+  select(matches("30")) %>%
+  select(contains("P"), -contains("01"))
+summary(routes_pspeeds30)
+routes_pspeed30 = rowSums(routes_pspeeds30)
+summary(routes_pspeed30)
+routes$routes_pspeed30 = routes_pspeed30
+# 40+ mph
+routes_pspeeds40 = routes %>%
+  select(matches("40")) %>%
+  select(contains("P"), -contains("01"))
+summary(routes_pspeeds40)
+routes_pspeed40 = rowSums(routes_pspeeds40)
+summary(routes_pspeed40)
+routes$routes_pspeed40 = routes_pspeed40
+
+# find percentage on change with different speeds:
+# 20- mph
+routes_fspeeds20 = routes %>%
+  select(matches("20")) %>%
+  select(contains("F"), -contains("01"))
+summary(routes_fspeeds20)
+routes_fspeed20 = rowSums(routes_fspeeds20)
+summary(routes_fspeed20)
+routes$routes_fspeed20 = routes_fspeed20
+# 30 mph
+routes_fspeeds30 = routes %>%
+  select(matches("30")) %>%
+  select(contains("F"), -contains("01"))
+summary(routes_fspeeds30)
+routes_fspeed30 = rowSums(routes_fspeeds30)
+summary(routes_fspeed30)
+routes$routes_fspeed30 = routes_fspeed30
+# 40+ mph
+routes_fspeeds40 = routes %>%
+  select(matches("40")) %>%
+  select(contains("F"), -contains("01"))
+summary(routes_fspeeds40)
+routes_fspeed40 = rowSums(routes_fspeeds40)
+summary(routes_fspeed40)
+routes$routes_fspeed40 = routes_fspeed40
 
 # sanity test of line E02000274 - E02000524
 rf_test = filter(rf_sf, id == "E02000274 E02000524")
@@ -31,10 +93,16 @@ rf_sfj = inner_join(rf_sf, routes, by = "id")
 rfu = rf_sfj %>% filter(Puptake > 0.1 & all11 >= 20 & all01 >= 20)
 # mapview::mapview(rfu)
 # routes with high levels of uptake have:
+mean(rfu$routes_pspeed20) / mean(routes$routes_pspeed20) # more 20mph roads in high uptake routes (expected)
+mean(rfu$routes_fspeed20) / mean(routes$routes_fspeed20) # less routes changed to 20 (unexpected)
+mean(rfu$routes_pspeed30) / mean(routes$routes_pspeed30) # less with 30 (expected)
+mean(rfu$routes_fspeed30) / mean(routes$routes_fspeed30) # less changed to 30 (expected)
+mean(rfu$routes_pspeed40) / mean(routes$routes_pspeed40) # much lower % on 40mph roads (expected)
+mean(rfu$routes_fspeed40) / mean(routes$routes_fspeed40) # less routes changed to 20 (expected)
 mean(rfu$Pmain40_N11) / mean(routes$Pmain40_N11) # less routes on main roads areas with high uptake (expected)
 mean(rfu$Fmain40_I) / mean(routes$Fmain40_I) # less fractional change in infra (unexpected)
 mean(rfu$Pcycleway11) / mean(routes$Pcycleway11) # less routes on cycleways (unexpected)
-mean(rfu$Fcycleway) / mean(routes$Fcycleway) # les routes on cycleways (unexpected)
+mean(rfu$Fcycleway) / mean(routes$Fcycleway) # less new cycleways (unexpected)
 
 # focus on speeds:
 rfu_fspeeds = rfu %>%
@@ -49,36 +117,54 @@ colMeans(rfu_fspeeds %>% st_set_geometry(NULL)) / colMeans(routes_pspeeds)
 
 routes.sub_vars = select(rfu, -Puptake, -id, -contains("bicycle"), -contains("percycle11"), -contains("all"),
                -contains("changecom"), -contains("P")) %>%
+  st_set_geometry(NULL) %>%
   select(matches("F|length|av"))
 train = routes.sub_vars %>%
   as.matrix()
 colnames(train)
-w = routes.sub$all11 + routes.sub$all01 / 2
-mx1 = xgboost(data = train, label = routes.sub$Puptake, weight = w, nrounds = 10)
-mx2 = xgboost(data = train, label = routes.sub$Puptake, weight = w, nrounds = 10,
-              params = list(booster = "gblinear")) # garbage results
+w = rfu$all11 + rfu$all01 / 2
+mx1 = xgboost(data = train, label = rfu$Puptake, weight = w, nrounds = 10)
 importance <- xgb.importance(model = mx1, feature_names = colnames(train))
 xgb.plot.importance(importance , top_n = 10)
 
-
-
+# run on full dataset ----
 sel_mincycle = routes$bicycle01 >= min_cycle & routes$all01 >= min_all &
-  routes$bicycle11 >= min_cycle & routes$all11 >= min_all
+  routes$bicycle11 >= 5
 summary(sel_mincycle)
 routes.sub <- routes[sel_mincycle,]
 routes.train <- sample_frac(routes.sub, 0.5)
 routes.test <- routes.sub[!routes.sub$id %in% routes.train$id,]
 
-# simple linear models
+# simple linear model
 summary(routes.sub)
-m1 = lm(Puptake ~ length + rf_avslope_perc + Fresidential20_N + Fcycleway, data =  routes.test)
+m1 = lm(Puptake ~ length + rf_avslope_perc + routes_pspeed20 + routes_pspeed30 + routes_pspeed40 + Fcycleway, data =  routes.sub)
+m2 = lm(Puptake ~ length + rf_avslope_perc + routes_fspeed20 + routes_fspeed30 + routes_fspeed40 + Fcycleway, data =  routes.sub)
+m3 = lm(Puptake ~ length + rf_avslope_perc + routes_infra_length : routes_pspeed20 +
+          routes_infra_length : routes_pspeed30 + routes_infra_length : routes_pspeed40 + Fcycleway : Pcycleway11, data =  routes.sub)
+m4 = lm(Puptake ~ length + rf_avslope_perc + routes_infra_length : routes_pspeed20 +
+          routes_infra_length : routes_pspeed30 + routes_infra_length : routes_pspeed40 + Pcycleway11, data =  routes.sub)
+m5 = lm(Puptake ~ length + rf_avslope_perc + routes_infra_length : routes_pspeed20
+        + routes_infra_length : routes_pspeed30 +
+        + routes_infra_length : routes_pspeed40, data =  routes.sub)
 summary(m1)
+summary(m2)
+summary(m3)
+summary(m4)
+summary(m5)
 
+routes.sim =  routes.sub %>%
+  mutate(routes_infra_length = routes_infra_length + 500,
+         routes_pspeed20 = routes_pspeed20 + 0.3,
+         routes_pspeed40 = routes_pspeed40 - 0.1
+         )
+puptake_sim = predict(m5, routes.sim)
+mean(puptake_sim)
+mean(routes.sub$Puptake) # ~ 1% increase in cycling...
 # explore feature importance with xgboost
 # routes.sub_vars = select(routes.sub, -Puptake, -id, -contains("bicycle"), -contains("percycle11"), -contains("all"),
 #                -contains("changecom"), -contains("P"))
 # % on busy
-routes.sub_vars = select(routes.sub, Pmain40_N11, Fmain40_N)
+routes.sub_vars = select(routes.sub, contains("routes_"))
 train = routes.sub_vars %>%
   as.matrix()
 colnames(train)
@@ -99,8 +185,6 @@ uptake_infra = predict(object = mx1, train_infra)
 message(paste0("This scenario results in an average of a ",
                round((mean(uptake_infra) - mean(routes.sub$Puptake)) * 100, 1),
                " percentage point increase in cycling"))
-
-#
 
 calc_up = function(x) {
   train_infra = select(routes.sub, -Puptake, -id, -contains("cycl"), -contains("all"), -contains("changecom")) %>%
