a51d624eadd0da19681670448560b107f299dbc0
Malcolm Morgan <mem48@users.noreply.github.com>
2018-03-14 16:05:15 +0000

    switch from real to double for costs

--- scripts/visualise/create_schemes_table.sql
+++ scripts/visualise/create_schemes_table.sql
@@ -1,14 +1,34 @@
-DROP TABLE IF EXISTS schemes;
+DROP TABLE schemes;
 DROP INDEX IF EXISTS schemes_gindx;
 
 CREATE TABLE IF NOT EXISTS schemes (
-		idGlobal INT PRIMARY KEY,
-		region VARCHAR(255) NULL,
-		schgroup INT NULL,
-		schtype VARCHAR(255) NULL,
-		length REAL NULL,
-		cost REAL NULL,
-		geotext GEOMETRY NOT NULL
+      idGlobal INT PRIMARY KEY,
+      groupid INT NOT NULL,
+      region VARCHAR(255) NULL,
+      cost REAL NULL,
+      costperperson REAL NULL,
+      ncyclebefore INT NULL,
+      ncycleafter INT NULL,
+      infratype VARCHAR(255) NULL,
+      change INT NULL,
+      per REAL NULL,
+      length REAL NULL,
+      ndrivebefore INT NULL,
+      ndriveafter INT NULL,
+      carkmbefore REAL NULL,
+      carkmafter REAL NULL,
+      carkm REAL NULL,
+      absenteeismbenefit DOUBLE NULL,
+      healthdeathavoided DOUBLE NULL,
+      healthbenefit DOUBLE NULL,
+      qualitybenefit DOUBLE NULL,
+      accidentsbenefit DOUBLE NULL,
+      co2saved DOUBLE NULL,
+      ghgbenefit DOUBLE NULL,
+      congestionbenefit DOUBLE NULL,
+      totalBen DOUBLE NULL,
+      costBenRatio REAL NULL,
+      geotext GEOMETRY NOT NULL
     );
 
 COPY schemes FROM '/home/malcolm/schemes.csv'  csv HEADER;
